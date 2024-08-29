22f230120c1898c7c96e489671a773c8ee70f5ca
FSennaMx <45613099+FSennaMx@users.noreply.github.com>
2024-08-29 17:00:53 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,10 +1,12 @@
-const http = require('http');
+// Create web server
+const http = require('http'); 
 
 function createWebServer() {
-    const server = http.createServer((req, res) => {
+    const server = http.createServer((req, res) => { 
         res.statusCode = 200;
+
         res.setHeader('Content-Type', 'text/plain');
-        res.end('Hello, World!\n');
+        res.end('Hello, World modified!\n');
     });
 
     server.listen(3000, '127.0.0.1', () => {
